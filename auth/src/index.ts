f27aa32234b00a918a22de4cfd8d51d44b75e2c4
Rangga Rifqi <rangga@ranggarifqi.com>
2022-03-31 19:51:32 +0700

    Setup mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import 'express-async-errors'; // This module will enable us to use `throw` keyword in an async route handler
+import "express-async-errors"; // This module will enable us to use `throw` keyword in an async route handler
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -24,6 +25,17 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log('Connected to MongoDB')
+  } catch (error) {
+    console.log(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("listening on port 3000");
+  });
+};
+
+start();

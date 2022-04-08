09d343b777c135465bc0832c498da552bd10f4f6
Rangga Rifqi <rangga@ranggarifqi.com>
2022-04-09 06:25:12 +0700

    Test for existing email

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,15 +1,19 @@
 import request from "supertest";
 
 import { app } from "../../app";
+import { User } from "../../models/User";
+import { ErrorResponse } from "../../commons/responses/error";
 
 it("returns a 201 on successful signup", async () => {
-  return request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "test123",
-    })
-    .expect(201);
+  const response = await request(app).post("/api/users/signup").send({
+    email: "test@test.com",
+    password: "test123",
+  });
+
+  expect(response.status).toBe(201);
+  expect(response.body.id).toBeDefined();
+  expect(response.body.email).toBe("test@test.com");
+  expect(response.body.password).toBeUndefined();
 });
 
 it("returns a 400 on invalid email", async () => {
@@ -19,16 +23,14 @@ it("returns a 400 on invalid email", async () => {
   });
 
   expect(response.status).toBe(400);
-  expect(response.text).toBe(
-    JSON.stringify({
-      errors: [
-        {
-          message: "Must be a valid email",
-          field: "email",
-        },
-      ],
-    })
-  );
+  expect(response.body).toMatchObject<ErrorResponse>({
+    errors: [
+      {
+        message: "Must be a valid email",
+        field: "email",
+      },
+    ],
+  });
 });
 
 describe("Test for failing password input", () => {
@@ -39,16 +41,14 @@ describe("Test for failing password input", () => {
     });
 
     expect(response.status).toBe(400);
-    expect(response.text).toBe(
-      JSON.stringify({
-        errors: [
-          {
-            message: "Password must be between 4 and 20 characters",
-            field: "password",
-          },
-        ],
-      })
-    );
+    expect(response.body).toMatchObject<ErrorResponse>({
+      errors: [
+        {
+          message: "Password must be between 4 and 20 characters",
+          field: "password",
+        },
+      ],
+    });
   });
 
   it("returns a 400 if password length is > 20", async () => {
@@ -58,15 +58,35 @@ describe("Test for failing password input", () => {
     });
 
     expect(response.status).toBe(400);
-    expect(response.text).toBe(
-      JSON.stringify({
-        errors: [
-          {
-            message: "Password must be between 4 and 20 characters",
-            field: "password",
-          },
-        ],
-      })
-    );
+    expect(response.body).toMatchObject<ErrorResponse>({
+      errors: [
+        {
+          message: "Password must be between 4 and 20 characters",
+          field: "password",
+        },
+      ],
+    });
+  });
+});
+
+it("returns a 400 if there's an existing email", async () => {
+  const existingUser = User.build({
+    email: "test@test.com",
+    password: "test123",
+  });
+  await existingUser.save();
+
+  const response = await request(app).post("/api/users/signup").send({
+    email: "test@test.com",
+    password: "test123",
+  });
+
+  expect(response.status).toBe(400);
+  expect(response.body).toMatchObject<ErrorResponse>({
+    errors: [
+      {
+        message: "User test@test.com already existed!",
+      },
+    ],
   });
 });

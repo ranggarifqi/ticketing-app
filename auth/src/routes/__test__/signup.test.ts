09d82b66b452a305ff9ac77db7e57c25dcd6e0ca
Rangga Rifqi <rangga@ranggarifqi.com>
2022-04-09 06:08:07 +0700

    Added more test

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,3 +11,62 @@ it("returns a 201 on successful signup", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 on invalid email", async () => {
+  const response = await request(app).post("/api/users/signup").send({
+    email: "test.com",
+    password: "test123",
+  });
+
+  expect(response.status).toBe(400);
+  expect(response.text).toBe(
+    JSON.stringify({
+      errors: [
+        {
+          message: "Must be a valid email",
+          field: "email",
+        },
+      ],
+    })
+  );
+});
+
+describe("Test for failing password input", () => {
+  it("returns a 400 if password length is < 4", async () => {
+    const response = await request(app).post("/api/users/signup").send({
+      email: "test@test.com",
+      password: "t",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.text).toBe(
+      JSON.stringify({
+        errors: [
+          {
+            message: "Password must be between 4 and 20 characters",
+            field: "password",
+          },
+        ],
+      })
+    );
+  });
+
+  it("returns a 400 if password length is > 20", async () => {
+    const response = await request(app).post("/api/users/signup").send({
+      email: "test@test.com",
+      password: "aaaaaaaaaaaaaaaaaaaaa",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.text).toBe(
+      JSON.stringify({
+        errors: [
+          {
+            message: "Password must be between 4 and 20 characters",
+            field: "password",
+          },
+        ],
+      })
+    );
+  });
+});

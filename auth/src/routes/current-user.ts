b74195364b662d8e64ce83694067a88399ff4fe9
Rangga Rifqi <rangga@ranggarifqi.com>
2022-06-12 15:32:53 +0800

    Update common package

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { jwtAuth } from "@ranggarp-ticketing/common";
+import { jwtAuth, RequestWithCredential } from "@ranggarp-ticketing/common";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", jwtAuth, (req, res) => {
+router.get("/api/users/currentuser", jwtAuth, (req: RequestWithCredential, res) => {
   return res.send({ currentUser: req.currentUser });
 });
 
